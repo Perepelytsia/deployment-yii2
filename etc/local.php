f3396acadb26790a8821a53f9d3cf37efcfafad4
Perepelytsia <perepelytsa@devoler.com>
2021-05-01 03:03:14 +0300

    change config

--- etc/local.php
+++ etc/local.php
@@ -10,18 +10,18 @@ return [
         ],
         'dbStatsFull' => [
             'class' => \app\components\DbConnection::class,
-            'dsn'      => 'mysql:host=localhost;dbname=x_slots_stats_full',
+            'dsn'      => 'mysql:host=mysql5.7;dbname=slots_stats_full',
             'username' => 'root',
             'password' => 'admin',
         ],
         'clickHouse' => function() {
             return new \app\components\ClickHouse([
-                'host' => '127.0.0.1',
+                'host' => 'clickhouse21.4',
                 'port' => '8123',
                 'username' => 'default',
                 'password' => '',
             ],[
-                'database' => 'sandmanslots',
+                'database' => 'slots',
             ]);
         },
     ]
