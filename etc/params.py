f3c521135cbc17f6e9d091f48563281e72a6c4ad
Perepelytsia <perepelytsa@devoler.com>
2021-05-07 18:58:07 +0300

    Add python config

---
+++ etc/params.py
@@ -0,0 +1,11 @@
+import os
+
+CLICKHOUSE_HOST = 'clickhouse21.4'
+CLICKHOUSE_PORT = '9000'
+CLICKHOUSE_USER = 'default'
+CLICKHOUSE_PASS = ''
+CLICKHOUSE_DB   = 'slots'
+MYSQL_HOT_HOST  = 'mysql5.7'
+MYSQL_HOT_USER  = 'root'
+MYSQL_HOT_PASS  = 'admin'
+MYSQL_HOT_DB    = 'slots'
